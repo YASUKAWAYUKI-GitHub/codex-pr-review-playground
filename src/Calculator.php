a11306253ecc224250385ecc9f4cbaff87e1e188
YASUKAWAYUKI-GitHub <atumu.4228@icloud.com>
2026-01-17 22:59:56 +0900

    正しいサンプルコード

---
+++ src/Calculator.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+class Calculator
+{
+    /**
+     * 割り算を行う
+     *
+     * @param int|float $a
+     * @param int|float $b
+     * @return float
+     * @throws InvalidArgumentException
+     */
+    public function divide(int|float $a, int|float $b): float
+    {
+        if ($b == 0.0) {
+            throw new InvalidArgumentException('0 で割ることはできません');
+        }
+
+        return $a / $b;
+    }
+}
